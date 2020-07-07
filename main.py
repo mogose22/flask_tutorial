e4e55f7adfca12164024b36dd1aa095b2ad61874
Yuriy Davydov <mogose22@gmail.com>
2020-07-07 17:50:16 +0400

    ex4: Удаление и обновление записей, фронт для этих действий

--- main.py
+++ main.py
@@ -55,6 +55,41 @@ def post_detail(post_id):
     print("Объект Article создан. Дальше возврат шаблона.")
     return render_template('post-detail.html', article=article)
     
+@app.route('/posts/<int:post_id>/delete')
+def post_delete(post_id):
+    print("Зашли в post_delete. post_id={}".format(post_id))
+    article = Article.query.get_or_404(post_id)
+    try:
+        print("пытаемся удалить запись {}".format(post_id))
+        db.session.delete(article)
+        print("пытаемся закоммитить изменения")
+        db.session.commit()
+        print("успешно! редирект на /posts")
+        return redirect('/posts')
+    except Exception as e:
+        return "При удалении статьи произошла ошибка {}".format(e)
+ 
+ 
+@app.route('/posts/<int:post_id>/update', methods=['POST', 'GET'])
+def post_update(post_id):
+    article = Article.query.get(post_id)
+    
+    if request.method == 'POST':
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+        print("update article.\ntitle: {}, intro: {}".format(article.title, article.intro))
+        try:
+            db.session.commit()
+            print('db.session.commit is ok!')
+            return redirect('/posts')
+        except Exception as e:
+            return "При редактировании статьи произошла ошибка: {} ".format(e)
+    else:
+        return render_template("post-update.html", article=article)
+
+
+    
 @app.route('/posts')
 def posts():
     articles = Article.query.order_by(Article.date.desc()).all()
